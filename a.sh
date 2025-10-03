a2b21414cec02179de8ff44bdb0bbd4a1c74754f
Samuel Moelius <sam@moeli.us>
2025-10-03 05:50:07 -0400

    Skip local validator

--- a.sh
+++ a.sh
@@ -1,20 +1,26 @@
 #! /bin/bash
 
+set -xe
+
 which solana-test-validator
 
 rm -rf test-ledger
 
 pushd foo && yarn && anchor build && popd
 
+PUBKEY="$(solana-keygen pubkey foo/target/deploy/foo-keypair.json)"
+sed -i "s/foo = \".*\"/foo = \"$PUBKEY\"/" foo/Anchor.toml
+sed -i "s/declare_id!(\".*\");/declare_id!(\"$PUBKEY\");/" foo/programs/foo/src/lib.rs
+
 RUST_LOG=debug "solana-test-validator" \
     "--ledger" \
     "test-ledger" \
     "--bpf-program" \
-    "3iKntdewJB8veY52mGrVMqLcAvBDgpGuPt7Ay1y6U3XU" \
+    "$PUBKEY" \
     "foo/target/deploy/foo.so" &
 
 pushd foo && anchor test --skip-deploy --skip-local-validator && popd
 
-pkill test-validator
+kill %%
 
 cat test-ledger/validator.log
