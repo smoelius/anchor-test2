d55d7a13883fb3facadce3806befb4269229a13f
Samuel Moelius <samuel.moelius@trailofbits.com>
2025-10-05 07:18:45 -0400

    No longer use sed to change program ids

--- a.sh
+++ a.sh
@@ -11,8 +11,6 @@ rm -f foo/target/deploy/foo-keypair.json
 pushd foo && yarn && anchor keys sync && anchor build && popd
 
 PUBKEY="$(solana-keygen pubkey foo/target/deploy/foo-keypair.json)"
-sed -i "s/foo = \".*\"/foo = \"$PUBKEY\"/" foo/Anchor.toml
-sed -i "s/declare_id!(\".*\");/declare_id!(\"$PUBKEY\");/" foo/programs/foo/src/lib.rs
 
 RUST_LOG=debug "solana-test-validator" \
     "--ledger" \
@@ -29,6 +27,6 @@ kill %%
 
 ls -l ../test-ledger
 
-head -n 1000 ../test-ledger/validator-*.log | grep DEBUG
+cat ../test-ledger/validator-*.log | grep DEBUG
 
 popd
